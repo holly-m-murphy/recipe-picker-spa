68818fb5a33679536a5ad861e5ac3d18a2b5b184
Holly Murphy <h_murphy@hollys-mbp-2.lan>
2018-05-03 20:25:07 -0500

    added unit tests for Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,21 +4,10 @@ import { Link } from 'react-router-dom'
 
 class Navigation extends Component {
 
-    state = {
-        addRecipeRedirect: false,
-        deleteRecipeRedirect: false,
-        findRecipe: false
-    }
-    // addRecipe = () => {
-    //     console.log(`updating`)
-    //     this.setState({
-    //         addRecipeRedirect: true
-    //     })
-    // }
+
+
     render() {
-        // if (this.state.addRecipeRedirect) {
-        //     return <Redirect push to="/add-recipe" />
-        // }
+
         return (
             <nav>
                 <div className="nav-wrapper light-blue darken-4">
@@ -28,18 +17,6 @@ class Navigation extends Component {
                         <li><a href=""><Link to="/remove-recipe"><i className="material-icons">highlight_off</i></Link></a></li>
                         <li><a href=""><Link to="/menu" ><i className="material-icons">restaurant_menu</i></Link></a></li>
                     </ul>
-
-                    {/*<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-                    <ul class="right hide-on-med-and-down">
-                        <li><a href="#" className="blue-text text-lighten-5">Add a Recipe</a></li>
-                        <li><a href="#" className="blue-text text-lighten-5">Remove a Recipe</a></li>
-                        <li><a href="#" className="blue-text text-lighten-5">Pick a Recipe</a></li>
-                    </ul>
-                    <ul class="side-nav" id="mobile-demo">
-                        <li><a href="#" className="blue-text text-lighten-5">Add a Recipe</a></li>
-                        <li><a href="#" className="blue-text text-lighten-5">Remove a Recipe</a></li>
-                        <li><a href="#" className="blue-text text-lighten-5">Pick a Recipe</a></li>
-                    </ul>*/}
 
                 </div>
             </nav>
--- src/components/Navigation.test.js
+++ src/components/Navigation.test.js
@@ -2,10 +2,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Navigation from './Navigation';
+import { Link } from 'react-router';
 
 describe("navigation component", async () => {
     it("should display the navigation", async () => {
         const wrapper = shallow(<Navigation />)
-        expect(true).toEqual(false)
+
+        expect(wrapper.find('Link').at(0).props().to).toEqual('/add-recipe')
+        expect(wrapper.find('Link').at(1).props().to).toEqual('/remove-recipe')
+        expect(wrapper.find('Link').at(2).props().to).toEqual('/menu')
     })
 })
+
+
