6e638b867f088191dbd2b19ec6f09ab72f3b0441
Holly Murphy <h_murphy@hollys-mbp-2.lan>
2018-05-03 20:45:58 -0500

    added unit test for RemoveRecipe component

--- src/components/RemoveRecipe.js
+++ src/components/RemoveRecipe.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 
-
 class RemoveRecipe extends Component {
+    state = {
+        searchText: '',
+        error: {
+            searchText: false
+        }
+    }
 
-
+    handleChange = (e) => {
+        console.log(`e.target`, e.target)
+        var change = {}
+        change[e.target.name] = e.target.value
+        this.setState(change)
+    }
 
     render() {
         return (
             <div className="">
                 <div className="row container additional-top-margin-button ">
                     <div className="input-field col s4 offset-s4">
-                        <textarea type="text" id="title-search " className="materialize-textarea remove-bottom-padding-and-margin" />
+                        <textarea type="text" id="title-search" name="searchText" className="materialize-textarea remove-bottom-padding-and-margin" onChange={this.handleChange} />
                         <label htmlFor="title-search remove-bottom-padding-and-margin">Search for a recipe: </label>
                     </div>
 
